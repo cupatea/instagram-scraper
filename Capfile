3f3cb51f153ed69ca731e83b5635ef1fe65ad684
Vladyslav Davydenko <vdavyden@gmail.com>
2019-03-16 16:17:35 +0200

    setup deploy

---
+++ Capfile
@@ -0,0 +1,15 @@
+# Load DSL and set up stages
+require "capistrano/setup"
+require "capistrano/deploy"
+require "capistrano/rbenv"
+require 'capistrano/rails'
+require 'capistrano/bundler'
+require 'capistrano/puma'
+install_plugin Capistrano::Puma
+require "capistrano/sidekiq"
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+require 'rollbar/capistrano3'
+
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+set :application, "instagram-scpraper"
+set :repo_url, "git@gitlab.com:cupatea/instagram-scraper"
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.2'
+
+set :passenger_restart_with_touch, true
+set :pty, true
+
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+# Default value for :linked_files is []
+set :linked_files, %w[config/database.yml
+                      config/sidekiq.yml
+                      config/master.key]
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w[log
+                     tmp/pids
+                     tmp/cache
+                     tmp/sockets
+                     vendor/bundle
+                     public/system
+                     node_modules
+                     storage]
+
+set :bundle_binstubs, nil
+
+set :keep_releases, 5
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc "Run rake yarn:install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
