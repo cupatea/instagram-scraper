506b37863c70dfda3c9f14f31169735f3a086719
Vladyslav Davydenko <vdavyden@gmail.com>
2019-03-17 22:22:16 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
+require "sidekiq/web"
+require "sidekiq/cron/web"
+
 Rails.application.routes.draw do
-  require "sidekiq/web"
-  require "sidekiq/cron/web"
-  mount Sidekiq::Web => "sidekiq"
+  # Devise for users and admins
+  devise_for :users,
+             path: "users",
+             controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords",
+                            confirmations: "users/confirmations", unlocks: "users/unlocks"  },
+             path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
+
+  devise_for :admins,
+             path: "admins",
+             controllers: { sessions: "admins/sessions", registrations: "admins/registrations", passwords: "admins/passwords",
+                            confirmations: "admins/confirmations", unlocks: "admins/unlocks"},
+             path_names: { sign_in: "login", sign_out: "logout" }
+  # Sidekick for root admin
+  authenticated :admin do
+    root "admins/dashboard#index"
+    authenticate :admin, ->(a) { a.root? } do
+      mount Sidekiq::Web => "admins/sidekiq"
+    end
+  end
 end
