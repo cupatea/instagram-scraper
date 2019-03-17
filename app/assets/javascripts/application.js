dae56b3728fa6131f2f79a1d9a89ffb3bb72d002
Vladyslav Davydenko <vdavyden@gmail.com>
2019-03-18 01:42:53 +0200

    bring turbolinks and ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
-//= require_tree .
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require_tree .
