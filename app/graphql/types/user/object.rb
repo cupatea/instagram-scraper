519f2982849b06dbbfd2a7b27b951093f3ce26f3
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-05 16:43:55 +0300

    move out user object tye from folder

--- app/graphql/types/user/object.rb
+++
@@ -1,6 +0,0 @@
-class Types::User::Object < Types::Base::Object
-  description 'User object'
-
-  field :email, String, null: false
-  field :uuid,  String, null: false
-end
---
+++ app/graphql/types/user_object.rb
@@ -0,0 +1,6 @@
+class Types::UserObject < Types::Base::Object
+  description 'User object'
+
+  field :email, String, null: false
+  field :uid,  String, null: false
+end
