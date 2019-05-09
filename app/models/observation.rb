79af53b4d49f9f47a4600a6812280859cf5c4916
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-09 14:42:01 +0300

    add validation for uniqueness of observation

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,6 @@
 class Observation < ApplicationRecord
   belongs_to :observer, polymorphic: true
   belongs_to :observee, polymorphic: true
+
+  validates :observee_id, uniqueness: { scope: [:observer_id, :observer_type, :observee_type] }
 end
