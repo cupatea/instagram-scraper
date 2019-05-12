1b496cb20b753be309a2d248e6b198096994c767
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-12 15:58:50 +0300

    fix typo

--- app/graphql/types/observation.rb
+++ app/graphql/types/observation.rb
@@ -1,11 +1,11 @@
 class Types::Observation < Types::Base::Object
   description 'Observation object'
 
-  field :id,             Integer,               null: false
-  field :observer_type,  String,                null: false
-  field :observer_id,    Integer,               null: false
-  field :observee_type,  String,                null: false
-  field :observee_id,    Integer,               null: false
-  field :observer,       Types::Token, null: false
-  field :observee,       Types::Observee, null: false
+  field :id,             Integer,          null: false
+  field :observer_type,  String,           null: false
+  field :observer_id,    Integer,          null: false
+  field :observee_type,  String,           null: false
+  field :observee_id,    Integer,          null: false
+  field :observer,       Types::Observer,  null: false
+  field :observee,       Types::Observee,  null: false
 end
