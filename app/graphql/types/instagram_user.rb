262f2237c8ec09f85a1b0453c414d4cd06ca980d
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-12 15:59:18 +0300

    implement filters by scrape_time

--- app/graphql/types/instagram_user.rb
+++ app/graphql/types/instagram_user.rb
@@ -4,5 +4,12 @@ class Types::InstagramUser < Types::Base::Object
   field :id,             Integer,                 null: false
   field :username,       String,                  null: false
   field :posts_count,    Integer,                 null: false
-  field :followers_data, [Types::FollowersDatum], null: false
+  field :followers_data, [Types::FollowersDatum], null: false do
+    argument :scrape_time_form, String, required: false, default_value: nil
+    argument :scrape_time_to,   String, required: false, default_value: nil
+  end
+
+  def followers_data(arguments_hash)
+    object.followers_data.scope_filter(arguments_hash)
+  end
 end
--- app/graphql/types/observee.rb
+++ app/graphql/types/observee.rb
@@ -2,7 +2,6 @@ class Types::Observee < Types::Base::Union
   description 'Object that can be observed, e.g. InstagramUser'
   possible_types Types::InstagramUser
 
-
   def self.resolve_type(_object, _context)
     Types::InstagramUser
   end
