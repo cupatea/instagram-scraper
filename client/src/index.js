fffc6b12a4f3d41623e09dd81729b049ac21e018
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-16 17:17:23 +0300

    add semantic ui

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
 import {ApolloProvider} from 'react-apollo';
+import 'semantic-ui-css/semantic.min.css'
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
