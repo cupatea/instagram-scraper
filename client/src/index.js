356b3b4bc630b6785991d6bc44689b3b1884eee2
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-16 16:06:55 +0300

    remove odd files; create basic structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {ApolloProvider} from 'react-apollo';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:5000/graphql',
+    headers: {
+      uid: localStorage.getItem('uid'),
+      client: localStorage.getItem('client'),
+      'access-token': localStorage.getItem('token'),
+      'token-type': 'Bearer',
+    },
+  }),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    Hello from Apolo React client
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
