8d6e0b15958df3d5561d10922a6d9de44e602fa2
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-17 12:06:20 +0300

    add page container component

---
+++ client/src/components/page-container.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import { Message } from '.'
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
+}))
+
+export default function PageContainer({messages=[], children}) {
+  const classes = useStyles()
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        {children}
+        {messages.map(message => <Message key={message.id} content={message.content} />)}
+      </div>
+    </Container>
+  )
+}
