b11392808d398cbd4b8137eb846fdef9686d8dd6
Vladyslav Davydenko <vdavyden@gmail.com>
2019-05-17 12:07:57 +0300

    replace semantic ui form wiht material form

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -1,60 +1,108 @@
-import React, {useState} from 'react'
-import {Segment, Button, Form, Message, Container } from 'semantic-ui-react'
-import {useSpring, animated} from 'react-spring'
+import React, { useState } from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default function LoginForm({ login, errors }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const onSubmit = event => {
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
+
+export default function LoginForm({ loginFunction }) {
+  const classes = useStyles()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(event) {
     event.preventDefault()
-    login({
+    loginFunction({
       variables: {
         email,
         password
       }
     })
   }
-  const fade = useSpring({ opacity: errors.length > 0 ? 1 : 0 })
 
   return (
-    <Container style={{ paddingTop: '20px'}}>
-      <Segment>
-        <animated.div style={fade}>
-          <Message>
-            <Message.Header>Action Forbidden</Message.Header>
-            <Message.List>
-            {
-              errors.map(error =>
-                 <Message.Item key={error}>
-                  {error}
-                 </Message.Item>
-              )
-            }
-            </Message.List>
-          </Message>
-        </animated.div>
-        <Form
-          onSubmit = { event => onSubmit(event) }>
-          <Form.Field style= { { paddingTop: '20px'} }>
-            <label>Email</label>
-            <input
-              type = 'email'
-              autoComplete = 'email'
-              onChange = { (event) => setEmail(event.target.value) }
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Password</label>
-            <input
-              type = 'password'
-              autoComplete = 'current-password'
-              onChange = { (event) => setPassword(event.target.value) }
-            />
-          </Form.Field>
+    <React.Fragment>
+      <Avatar className={classes.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <form
+        className={classes.form}
+        noValidate
+        onSubmit = { (event) => handleSubmit(event) }
+      >
 
-          <Button type='submit'>Submit</Button>
-        </Form>
-      </Segment>
-    </Container>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          onChange = { event => setEmail(event.target.value) }
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange = { event => setPassword(event.target.value) }
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <Link href="#" variant="body2">
+              Forgot password?
+            </Link>
+          </Grid>
+          <Grid item>
+            <Link href="#" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </React.Fragment>
   )
 }
