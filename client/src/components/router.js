3f7e2e1d404ecc5c14fded8c36f67f96ab860149
Vladyslav Davydenko <vdavyden@gmail.com>
2019-06-30 20:12:11 +0300

    add router component

---
+++ client/src/components/router.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Router as ReachRouter, Redirect} from '@reach/router'
+import {useSpring, animated} from 'react-spring'
+
+import {DashboardPage, LoginPage, SignUpPage} from '../pages'
+import {isTokens as isLoggedIn} from '../utils'
+
+export const routes = {
+  rootPath:          '/',
+  dashboardPagePath: '/dashboard',
+  loginPagePath:     '/login',
+  signUpPagePath:    'sign-up',
+}
+
+export default function Router(){
+  const transition = useSpring({opacity: 1, from:{opacity: 0}})
+
+  const resolveRedirectPath =
+    isLoggedIn()
+      ? routes.dashboardPagePath
+      : routes.loginPagePath
+
+  return(
+    <animated.div style={transition} >
+      <ReachRouter primary={true}>
+        <Redirect
+         from={routes.rootPath}
+         to={resolveRedirectPath}
+         noThrow
+       />
+
+        <LoginPage path={routes.loginPagePath}/>
+        <SignUpPage path={routes.signUpPagePath}/>
+        <DashboardPage path={routes.dashboardPagePath} />
+      </ReachRouter>
+    </animated.div>
+  )
+}
