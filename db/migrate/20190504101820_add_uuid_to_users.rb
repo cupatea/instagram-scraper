672191ecf3f8903934bccf5bfd3eddc47cd47dba
Vladyslav Davydenko <vdavyden@gmail.com>
2019-07-07 18:21:20 +0300

    fix failed migration

--- db/migrate/20190504101820_add_uuid_to_users.rb
+++ db/migrate/20190504101820_add_uuid_to_users.rb
@@ -1,6 +1,6 @@
 class AddUuidToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :uuid, :string, null: false
+    add_column :users, :uuid, :string
     add_index :users, :uuid
   end
 end
