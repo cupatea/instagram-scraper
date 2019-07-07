d7c61d4e3d8bfcebcb42404bad425976c84ef86c
Vladyslav Davydenko <vdavyden@gmail.com>
2019-07-07 22:11:03 +0300

    add gmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,13 @@ Rails.application.configure do
 
   # Devise mailer
   config.action_mailer.default_url_options = { host: 'instat.cf' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            Rails.application.credentials.gmail_username,
+    password:             Rails.application.credentials.gmail_password,
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
