f6631217bddf92c165ca8b35cb654ba12b5fc140
Vladyslav Davydenko <vdavyden@gmail.com>
2019-07-07 21:53:07 +0300

    remove credentials for redundant development; bring back routes for devise users

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,21 @@ Rails.application.routes.draw do
   devise_for :admins,
              path: "admins",
              controllers: { sessions: "admins/sessions",
-                            registrations: "admins/registrations",
                             passwords: "admins/passwords",
-                            confirmations: "admins/confirmations",
-                            unlocks: "admins/unlocks" },
-             path_names: { sign_in: "login", sign_out: "logout" }
+                            confirmations: "admins/confirmations" },
+             path_names: { sign_in: "login",
+                           sign_out: "logout" }
+
+  devise_for :users,
+             path: "users",
+             controllers: { sessions: "users/sessions",
+                            registrations: "users/registrations",
+                            passwords: "users/passwords",
+                            confirmations: "users/confirmations",
+                            unlocks: "users/unlocks" },
+             path_names: { sign_in: "login",
+                           sign_out: "logout",
+                           sign_up: "register" }
   # Sidekick for root admin
   authenticated :admin do
     authenticate :admin, ->(a) { a.root? } do
