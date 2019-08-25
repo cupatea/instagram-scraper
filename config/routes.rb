a4eafc774951267ebd0370057ad957d2751fc89f
Vladyslav Davydenko <vdavyden@gmail.com>
2019-08-25 16:28:52 +0300

    add Rack::Auth for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,14 @@
 require "sidekiq/web"
 require "sidekiq/cron/web"
 
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == Rails.application.credentials.sidekiq_username &&
+      password == Rails.application.credentials.sidekiq_password
+  end
+end
+
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  # Devise for users and admins
-  devise_for :admins,
-             path: "admins",
-             controllers: { sessions: "admins/sessions",
-                            passwords: "admins/passwords",
-                            confirmations: "admins/confirmations" },
-             path_names: { sign_in: "login",
-                           sign_out: "logout" }
-
-  devise_for :users,
-             path: "users",
-             controllers: { sessions: "users/sessions",
-                            registrations: "users/registrations",
-                            passwords: "users/passwords",
-                            confirmations: "users/confirmations",
-                            unlocks: "users/unlocks" },
-             path_names: { sign_in: "login",
-                           sign_out: "logout",
-                           sign_up: "register" }
-  # Sidekick for root admin
-  authenticated :admin do
-    authenticate :admin, ->(a) { a.root? } do
-      mount Sidekiq::Web => "/"
-    end
-  end
+  mount Sidekiq::Web => "/"
 end
