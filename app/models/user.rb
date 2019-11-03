0f9c8d5310576d2719a89e989d5ab762d74caa3d
Vladyslav Davydenko <vdavyden@gmail.com>
2019-11-03 15:11:07 +0200

    fix checking validates_associated instagram_users on every login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :instagram_users, as: :observee, through: :observations, source: :observee, source_type: 'InstagramUser'
 
   validates :username, uniqueness: true
-  validates_associated :instagram_users, message: 'username does not exit on Instagram'
+  validates_associated :instagram_users, message: 'username does not exit on Instagram', unless: :persisted?
 
   def confirm_now!
     update confirmed_at: Time.zone.now
