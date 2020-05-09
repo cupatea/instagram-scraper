f4a33cf0010a8dc721e3059f418eb7c51f2d4ac4
Vladyslav Davydenko <vdavyden@gmail.com>
2020-05-09 22:32:33 +0300

    [vladyslav] fix deploy url

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'instat.cf', user: "deploy", roles: %w{app db web}
+server 'instat.net', user: "deploy", roles: %w{app db web}
 set :branch,    'master'
 set :rails_env, 'production'
 set :deploy_to, '/home/deploy/www/instagram-scraper/production'
